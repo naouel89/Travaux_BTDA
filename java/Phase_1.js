7ee68945d5bd7e8286a1d8221ca3382ee43047b4
naouel21 <jessus.n@gmail.com>
2023-03-21 15:00:47 +0100

    Jardi_tableau

---
+++ java/Phase_1.js
@@ -0,0 +1,11 @@
+Variables N, i en Entier
+Debut
+Ecrire "Entrez un nombre : "
+Lire N
+Stop ← N+10
+Ecrire "Les 10 nombres suivants sont : "
+TantQue N < Stop
+   N ← N+1
+   Ecrire N
+FinTantQue
+Fin
