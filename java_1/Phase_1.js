de1bbc80dff68310749c8be870c9554cf73365eb
naouel21 <jessus.n@gmail.com>
2023-03-27 14:01:44 +0200

    exo_js_2

--- java_1/Phase_1.js
+++ java_1/Phase_1.js
@@ -1,3 +1,6 @@
 var nom = window.prompt( "saisissez votre nom" );
+var prenom = window.prompt( "saisissez votre prénom" );
+if (window.confirm("Voulez-vous sure ?") == true) 
 alert( " votre nom : " + nom );
-document.write( nom );
+alert( " votre prénom : " + prenom );
+document.write( nom + prenom);
