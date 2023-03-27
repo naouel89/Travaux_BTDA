720ae93759104ffd6a488cc805c291d9c5ff841f
naouel21 <jessus.n@gmail.com>
2023-03-27 15:55:01 +0200

    js_exercice3

--- java_2/phase_2.js
+++ java_2/phase_2.js
@@ -1,1 +1,11 @@
-console.log( " test connect " )
+alert( "Bonjour" );
+var a;
+var b;
+var s;
+
+a = prompt ( "Entré la valeur a", a);
+a = parseInt ( a );
+b = prompt ( "entré la valeur b", b);
+b= parseInt ( b );
+s = a + b;
+alert ( " égale à : " + s ) 
