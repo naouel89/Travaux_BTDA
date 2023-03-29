c732ddfb6c0850cf90cf8523499d0cb31264797e
naouel21 <jessus.n@gmail.com>
2023-03-29 16:00:23 +0200

    Calculatrice2

--- javascript/java_7/phase_7(3).js
+++ javascript/java_7/phase_7(3).js
@@ -1,44 +1,26 @@
-var nombre1 = window.prompt("saisir un chiffre entier :");
-nombre1 = parseInt(nombre1);
-var operation = window.prompt("quelles opération voulez vous faire ?    " + "   1 = addition  " + " 2 = soustraction   " + "   3 = multiplication    " + " 4 = division   ");
-var nombre2 = window.prompt("saisir un autre chiffre entier :");
-nombre2 = parseInt(nombre2);
-
-operation = parseInt(operation);
-
-
-if (operation < 1 || operation > 4) 
-{
-    alert("Erreur!");
-    location.reload();
-}
-else 
-{
-    if (operation == 1) {
-        operation = nombre1 + nombre2;
-        alert("vous avez fait une addiction = " + operation);
-        location.reload();
-    }
-    else if (operation == 2) {
-        operation = nombre1 - nombre2;
-        alert("vous avez fait une soustraction = " + operation);
-        location.reload();
-    }
-    else if (operation == 3) 
+function rslt()
+{  
+    var nombr1 = document.forms["calculatrice"].nb1.value;
+    var nombr2 = document.forms["calculatrice"].nb2.value;
+    var resultat;
+     
+    switch (document.calculatrice.operateur.value)
     {
-        operation = nombre1 * nombre2;
-        alert("vous avec fait une multiplication = " + operation);
-        location.reload();
+        case '+':
+        resultat =parseFloat(nombr1) + parseFloat(nombr2) ;
+        break;
+        case '-':
+        resultat =parseFloat(nombr1) - parseFloat(nombr2);
+        break;
+        case '*':
+        resultat =parseFloat(nombr1) * parseFloat(nombr2);
+        break;
+        case '/':
+        resultat =parseFloat(nombr1) / parseFloat(nombr2);
+        break;
+        default:
+        document.calculatrice.mess.value = "Verifier votre operation";
+        break;
     }
-    else if (operation == 4) 
-        if (nombre1==0 || nombre2==0)
-        {
-            alert("impossible");
-            location.reload();
-        }
-    else{
-        operation = nombre1 / nombre2;
-        alert("vous avec fait une division = " + operation );
-        location.reload();
-    }
+    document.forms["calculatrice"].ecran.value = resultat;
 }
