b20b17377be2eed4e37f2e3c8314daa871aa6acb
naouel21 <jessus.n@gmail.com>
2023-03-29 14:55:27 +0200

    calculatrice

--- javascript/java_7/phase_7(3).js
+++ javascript/java_7/phase_7(3).js
@@ -1,7 +1,44 @@
-var nombre1 = window.prompt("saisir un chiffre :");
+var nombre1 = window.prompt("saisir un chiffre entier :");
 nombre1 = parseInt(nombre1);
-var operation = window.prompt("quelles opération voulez vous faire ?" + "1 = addition" + "2 = soustraction" + "3 = multiplication" + "4 = division");
-operation = parseInt (operation);
-var nombre2 = window.prompt("saisir un autre chiffre");
+var operation = window.prompt("quelles opération voulez vous faire ?    " + "   1 = addition  " + " 2 = soustraction   " + "   3 = multiplication    " + " 4 = division   ");
+var nombre2 = window.prompt("saisir un autre chiffre entier :");
 nombre2 = parseInt(nombre2);
-var resultat = parseInt(resultat)
+
+operation = parseInt(operation);
+
+
+if (operation < 1 || operation > 4) 
+{
+    alert("Erreur!");
+    location.reload();
+}
+else 
+{
+    if (operation == 1) {
+        operation = nombre1 + nombre2;
+        alert("vous avez fait une addiction = " + operation);
+        location.reload();
+    }
+    else if (operation == 2) {
+        operation = nombre1 - nombre2;
+        alert("vous avez fait une soustraction = " + operation);
+        location.reload();
+    }
+    else if (operation == 3) 
+    {
+        operation = nombre1 * nombre2;
+        alert("vous avec fait une multiplication = " + operation);
+        location.reload();
+    }
+    else if (operation == 4) 
+        if (nombre1==0 || nombre2==0)
+        {
+            alert("impossible");
+            location.reload();
+        }
+    else{
+        operation = nombre1 / nombre2;
+        alert("vous avec fait une division = " + operation );
+        location.reload();
+    }
+}
